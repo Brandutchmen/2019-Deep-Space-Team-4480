09ff381a1a4bbf12040e9efe89f7213c5de31915
Brandon Young <brandony@discjockeynews.com>
2019-01-10 17:41:26 -0600

    init
    from DTD

---
+++ robot.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import wpilib
+from wpilib import drive
+import time
+import ctre
+
+from robotpy_ext.autonomous import AutonomousModeSelector
+from robotpy_ext.common_drivers import units, navx
+
+class MyRobot(wpilib.IterativeRobot):
+
+    def robotInit(self):
+
+        #Motor Init
+        self.motor1 = ctre.WPI_TalonSRX(1)
+        self.motor2 = ctre.WPI_TalonSRX(2)
+        self.motor3 = ctre.WPI_TalonSRX(3)
+        self.motor4 = ctre.WPI_TalonSRX(4)
+
+        #Sensor Init
+        self.ultrasonic = wpilib.AnalogInput(0)
+        self.distanceSensor = navx.AHRS.create_i2c()
+
+        #Controller Init
+        self.playerOne = wpilib.XboxController(0)
+
+        #Navx Init
+        self.navx = navx.AHRS.create_spi()
+
+        #Setting Drive
+        self.robotDrive = wpilib.RobotDrive(self.motor1, self.motor2, self.motor3, self.motor4)
+
+    def disabledInit(self):
+        pass
+
+    def autonomousInit(self):
+        pass
+
+    def autonomousPeriodic(self):
+        pass
+
+    def teleopInit(self):
+        #self.navx.reset()
+        pass
+
+    def teleopPeriodic(self):
+
+        if self.ultrasonic.getVoltage() < 4.9:
+            print("Close Range - Object Detected")
+        else:
+            print("No Object")
+
+
+        if self.playerOne.getAButton():
+            self.autoAlign()
+        elif self.playerOne.getBButton():
+            self.navx.reset()
+        else:
+            self.robotDrive.arcadeDrive(-self.playerOne.getY(0), self.playerOne.getX(0))
+
+    def autoAlign(self):
+        speed = 0.5
+        if self.navx.getYaw() > 0 and not self.navx.getYaw() < -170 or self.navx.getYaw() > 170:
+            self.robotDrive.arcadeDrive(-self.playerOne.getY(0), speed)
+        elif self.navx.getYaw() < 0 and not self.navx.getYaw() < -170 or self.navx.getYaw() > 170:
+            self.robotDrive.arcadeDrive(-self.playerOne.getY(0), -speed)
+        elif self.navx.getYaw() < -170 or self.navx.getYaw() > 170:
+            self.robotDrive.arcadeDrive(-self.playerOne.getY(0), 0)
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
---
+++ todo.md
@@ -0,0 +1,12 @@
+#Todo for Hud
+*Elevation
+*Vision (Front, Rear) - 360?
+*isCargoLoaded?
+*IR sensor on line?
+*Crosshair
+
+#Nice to have:
+*Live Scores
+*Auto AutonomousModeSelector
+*Ramp Deploy
+*Timer
